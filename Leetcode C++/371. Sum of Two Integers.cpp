e3b6aa03eda97aa36e89ef18de36e6bbac966cb2
dubeyrati <95397109+dubeyrati@users.noreply.github.com>
2023-08-16 18:51:51 +0530

    Add files via upload

---
+++ Leetcode C++/371. Sum of Two Integers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+         int p = max(a,b);
+        int q = min(a,b);
+        int x=q;
+        while(x!=0)
+        {
+            cout<<p<<" ";
+            x = p%q;
+            int divi = p/q;
+            p = (-(~divi))*q;
+            q = x;
+        }
+        return p;
+    }
+};
