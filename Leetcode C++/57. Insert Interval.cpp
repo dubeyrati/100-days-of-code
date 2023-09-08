f3cefdf4de29e63858fcbce5556ce5feb9f86ce3
dubeyrati <95397109+dubeyrati@users.noreply.github.com>
2023-09-08 19:07:45 +0530

    Add files via upload

---
+++ Leetcode C++/57. Insert Interval.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+     vector<vector<int>> ans;
+
+        sort(intervals.begin(), intervals.end()); 
+
+        for(auto it: intervals)
+        {
+            if(it[1] < newInterval[0]) 
+                ans.push_back(it);
+
+            else if(newInterval[1] < it[0]) 
+            {
+                ans.push_back(newInterval);
+                newInterval = it;
+            }
+            else 
+            {
+                newInterval[0] = min(newInterval[0], it[0]);
+                newInterval[1] = max(newInterval[1], it[1]);
+            } 
+        }
+        
+        ans.push_back(newInterval);
+
+        return ans;    
+    }
+};
