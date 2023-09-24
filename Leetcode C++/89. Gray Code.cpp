db05749ba341b2205f24ecd29e235ff6112b5483
Rati Dubey <95397109+dubeyrati@users.noreply.github.com>
2023-09-24 22:49:58 +0530

    Add files via upload

---
+++ Leetcode C++/89. Gray Code.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    inline int toggle(int n, int i) {
+        return n ^ (1 << i);
+    }
+    int next(int n, unordered_set<int> s) {
+        int ans = 0;
+        for (int i = 0; i < 32; ++i) {
+            ans = toggle(n, i);
+            if (s.find(ans) == s.end()) break;
+        }
+        return ans;
+    }
+public:
+    vector<int> grayCode(int n) {
+        unordered_set<int> s{0};
+        vector<int> ans(pow(2, n), 0);
+        for (int i = 1; i < ans.size(); ++i) {
+            s.insert(ans[i] = next(ans[i - 1], s));
+        }
+        return ans;
+    }
+};
